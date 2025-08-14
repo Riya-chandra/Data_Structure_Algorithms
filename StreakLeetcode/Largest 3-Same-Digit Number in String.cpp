216fdd41b2633427da4d0aeb24720e7215599ab2
riya chandra <riyachandra9119@gmail.com>
2025-08-15 00:13:29 +0530

    pofd

---
+++ StreakLeetcode/Largest 3-Same-Digit Number in String.cpp
@@ -0,0 +1,14 @@
+using namespace std;
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        string ans = "";
+        for (int i = 0; i <= num.size() - 3; i++) {
+            if (num[i] == num[i+1] && num[i] == num[i+2]) {
+                string curr = num.substr(i, 3);
+                if (curr > ans) ans = curr; 
+        }
+    }
+        return ans;
+    }
+};
