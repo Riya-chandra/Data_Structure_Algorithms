0105f31ce3cf35796da05be70131a08c097f9aaf
riya chandra <riyachandra9119@gmail.com>
2025-10-11 00:33:24 +0530

    pofd

---
+++ StreakLeetcode/Taking Maximum Energy From the Mystic Dungeon.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n = energy.size();
+        vector<int> dp(n, 0);
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (i + k < n)
+                dp[i] = energy[i] + dp[i + k];
+            else
+                dp[i] = energy[i];
+        }
+
+        return *max_element(dp.begin(), dp.end());
+    }
+};
