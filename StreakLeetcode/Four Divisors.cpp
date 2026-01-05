fb747c05bb7f9cf552a456e0a0120b26f77e0821
riya chandra <riyachandra9119@gmail.com>
2026-01-05 13:19:26 +0530

    aaded

---
+++ StreakLeetcode/Four Divisors.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int ans = 0;
+        for (int x : nums) {
+        int sum=0;
+        int count=0;
+     for(int j=1;j*j<=x;j++){
+        if(x%j==0){
+            count ++;
+            sum+=j;
+            if(j!=x/j){
+                count++;
+                sum+=x/j;
+            }
+            if(count>4) break;
+        }
+     }
+     if(count==4){
+        ans+=sum;
+     }
+        }
+        return ans;
+    }
+};
